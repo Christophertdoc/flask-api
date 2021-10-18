8dda281ce63379c43680924cbb65bb27c7f32e8a
Chris Dougherty <chris@wgacany.com>
2021-10-18 19:14:34 -0400

    params

--- main.py
+++ main.py
@@ -4,15 +4,25 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
+names = {
+  "Ella": {
+    "age": 4,
+    "food": "peanut butter"
+  },
+  "Linus": {
+    "age": 6,
+    "food": "sweet potato"
+  }
+}
 
 class HelloElla(Resource):
-  def get(self):
-    return {"data": "Ella, can you get me a Hello."}
+  def get(self, name):
+    return names[name]
 
   def post(self):
     return {"data": "Ella, can you post up."}
 
-api.add_resource(HelloElla, "/helloella")
+api.add_resource(HelloElla, "/helloella/<string:name>")
  
 if __name__ == "__main__":
   app.run(debug=True)
--- test.py
+++ test.py
@@ -2,5 +2,5 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.post(BASE + "helloella")
+response = requests.get(BASE + "helloella/Ella")
 print(response.json())
