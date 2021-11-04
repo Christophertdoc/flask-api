1026918bdcfbdf248b06d3093295404234bd2373
Chris Dougherty <chris@wgacany.com>
2021-11-04 18:02:58 -0400

    add delete functionality

--- test.py
+++ test.py
@@ -2,8 +2,19 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.put(BASE + "video/1", {"likes": 10, "name": "Ella and Her Treats", "views": 2000})
-print(response.json())
+data = [
+        {"likes": 79, "name": "Tam", "views": 2000},
+        {"likes": 12, "name": "Captain Lamb", "views": 202},
+        {"likes": 10, "name": "Ella and Her Treats", "views": 2030}
+      ]
+
+for i in range(len(data)):
+  response = requests.put(BASE + "video/" + str(i), data[i])
+  print(response.json())
+
+
+response = requests.delete(BASE + "video/0")
+print(response)
 
 input()
 
